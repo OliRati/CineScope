988b7297d42e1951125e1b0ba5bb340308473a3f
Olivier RATIEVILLE <olivier.ratieville@gmail.com>
2026-02-16 14:20:51 +0100

    Modification avatar

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\AvatarUploadFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -16,15 +20,39 @@ final class ProfileController extends AbstractController
             'user' => $user,
         ]);
     }
-     #[Route('/profile/avatar', name: 'app_profile_avatar')]
-    public function avatar(): Response
+    #[Route('/profile/avatar', name: 'app_profile_avatar')]
+    public function avatar(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $avatarFile = $form->get(name: 'avatar')->getData();
-        $user = $this->getUser();
-        return $this->render('profile/index.html.twig', [
-            'user' => $user,
+        $avatarFile = "";
+        $form = $this->createForm(AvatarUploadFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $avatarFile = $form->get(name: 'avatar')->getData();
+
+            if ($avatarFile) {
+                $newFilename = uniqid() . '.' . $avatarFile->guessExtension();
+
+                $avatarFile->move(
+                    $this->getParameter('avatars_directory'),
+                    $newFilename
+                );
+
+                $securityUser = $this->getUser();
+                $userRepository = $entityManager->getRepository(User::class);
+                $user = $userRepository->findOneBy(['email' => $securityUser->getUserIdentifier()]);
+
+                $user->setAvatar($newFilename);
+
+                $entityManager->persist($user);
+                $entityManager->flush();
+      
+                return $this->redirectToRoute('app_profile');
+            }
+        }
+
+        return $this->render('profile/avatar.html.twig', [
+            'avatarForm' => $form
         ]);
     }
-
-    
 }
